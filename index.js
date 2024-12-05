f09265978fd7e85afc459a53a3421b25902f9e58
pc-miki <miguelpereira3005@gmail.com>
2024-12-05 02:00:39 -0300

    chupalo vicho

--- index.js
+++ index.js
@@ -28,4 +28,95 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/administracion', (req, res) =>{
+    res.render('administracion')
+})
+
+
+////////    CONSULTAS ////////////////////
+
+
+app.get('/consulta1', (req, res) =>{
+    res.render('consulta1')
+})
+
+
+app.get('/consulta2', (req, res) =>{
+    res.render('consulta2')
+})
+
+
+app.get('/consulta3', (req, res) =>{
+    res.render('consulta3')
+})
+
+
+
+app.get('/consulta4', (req, res) =>{
+    res.render('consulta4')
+})
+
+
+
+app.get('/consulta5', (req, res) =>{
+    res.render('consulta5')
+})
+
+
+
+app.get('/consulta6', (req, res) =>{
+    res.render('consulta6')
+})
+
+
+
+app.get('/consulta7', (req, res) =>{
+    res.render('consulta7')
+})
+
+
+
+
+app.get('/consulta8', (req, res) =>{
+    res.render('consulta8')
+})
+
+
+
+app.get('/consulta9', (req, res) =>{
+    res.render('consulta9')
+})
+
+
+
+
+app.get('/consulta10', (req, res) =>{
+    res.render('consulta10')
+})
+
+
+
+app.get('/consulta11', (req, res) =>{
+    res.render('consulta11')
+})
+
+
+app.get('/consulta12', (req, res) =>{
+    res.render('consulta12')
+})
+
+
+app.get('/consulta13', (req, res) =>{
+    res.render('consulta13')
+})
+
+
+app.get('/consulta14', (req, res) =>{
+    res.render('consulta14')
+})
+
+app.get('/consulta15', (req, res) =>{
+    res.render('consulta15')
+})
+
 app.listen(3000 , () => console.log('tuki'));
